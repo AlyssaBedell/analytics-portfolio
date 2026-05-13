d7f69695b007ffc6b59f0dcbe1ac774cf055b145
Alyssa Bedell <alyssabedell.98@gmail.com>
2026-05-13 17:42:41 -0400

    Updates

---
+++ _next/static/8lIyX5PXlEyYiKRnQlR3O/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/analytics-portfolio//_next/:path+",
+        "destination": "/analytics-portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
